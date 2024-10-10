e0cad42a74d5e683923095031d3466b05735fe28
Salah Tamer <ma9060139@gmail.com>
2024-10-10 04:25:59 +0300

    Binary Search

---
+++ 03 Binary Search/HW 01 - 4 Medium Challenges/03_Valid Triangle Number.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+    //lower bound: find the first greater than of equal index
+    int BinarySearch(vector<int> &nums, int start, int target){
+        int end = nums.size() - 1;
+        int position = nums.size();
+
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if(nums[mid] >= target){
+                position = mid;
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+
+        return position;
+    }
+public:
+    int triangleNumber(vector<int>& nums) {
+        int len = nums.size();
+        sort(nums.begin(), nums.end());
+
+        int count = 0;
+        for(int i = 0; i < len - 2; i++){
+            for(int j = i + 1; j < len - 1; j++){
+                int k = j + 1;
+
+                int first_idx = BinarySearch(nums, k, nums[i] + nums[j]);
+                count += first_idx - j - 1;
+            }
+        }
+
+        return count;
+    }
+};
+
+int main(){
+
+}
